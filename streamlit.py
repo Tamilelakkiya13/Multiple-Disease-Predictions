faa7a9a95b27f20c129b47ebdb34a87ffddd9b76
Tamilelakkiya13 <tamilnirmala13nov@gmail.com>
2025-01-10 13:11:45 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pickle
 import numpy as np
 import pandas as pd
-import xgboost as xgb
+import xgboost 
 #import sklearn
 # Load the trained Liver Disease model
 with open("liver_model.pkl", "rb") as file:
