ae63d3a507cd88ba19d5f893a4e74f985c2ddba4
Tamilelakkiya13 <tamilnirmala13nov@gmail.com>
2025-01-10 13:22:40 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -2,8 +2,6 @@ import streamlit as st
 import pickle
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
-from xgboost import XGBClassifier
 
 # Load the trained Liver Disease model
 with open("liver_model.pkl", "rb") as file:
