d1236bdb0abd44a1e4e195b960c6bdc77e4e4561
Tamilelakkiya13 <tamilnirmala13nov@gmail.com>
2025-01-12 17:55:37 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -2,9 +2,6 @@ import streamlit as st
 import pickle
 import numpy as np
 import pandas as pd
-import xgboost as xgb
-from sklearn.ensemble import RandomForestClassifier 
-import os
 
 # Load the trained models with error handling
 try:
